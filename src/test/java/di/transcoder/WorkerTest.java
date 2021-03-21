68cffdf6fdcaf68f75ab599b34bb332d3014ff66
taehun.lee <taehun.lee@meshkorea.net>
2021-03-21 13:56:03 +0900

    [step5] Worker run 테스트

---
+++ src/test/java/di/transcoder/WorkerTest.java
@@ -0,0 +1,15 @@
+package di.transcoder;
+
+import org.junit.jupiter.api.Test;
+
+class WorkerTest {
+
+  @Test
+  public void shouldRunSuccessfully() {
+//    JobQueue mockJobQueue = new MockJobQueue();
+//    Transcoder mockTranscoder = new MockTranscoder();
+
+//    Worker worker = new Worker(mockJobQueue, mockTranscoder);
+//    worker.run();
+  }
+}
